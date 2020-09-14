154aaad3bd2aa97cd55f74680e79958688b46d70
Ítalo Silveira <italobrian@gmail.com>
2020-09-14 20:18:58 -0300

    spa renamed

--- src/BAYSOFT.Presentations.Web.SPA.StockWallet/Program.cs
+++ src/BAYSOFT.Presentations.Web.SPA.StockWallet/Program.cs
@@ -1,13 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace BAYSOFT.Presentations.Web.SPA.StockWallet.ReactJS
+namespace BAYSOFT.Presentations.Web.SPA.StockWallet
 {
     public class Program
     {
